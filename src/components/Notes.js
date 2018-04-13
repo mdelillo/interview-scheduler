15a4dcd6f4a31343c2b7a076896dd5ec163e4b32
Mark DeLillo <mdelillo@pivotal.io>
2018-04-13 17:34:47 -0400

    Add spinner to Notes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -2,29 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import { firebaseConnect } from 'react-redux-firebase';
+import { firebaseConnect, isLoaded } from 'react-redux-firebase';
+import Spinner from './Spinner';
 
+const Notes = ({ firebase, notes }) => {
+  if (!isLoaded(notes)) {
+    return <Spinner text="Loading notes" />;
+  }
 
-const Notes = ({ firebase, notes }) => (
-  <div className="Notes">
-    <h2>Notes</h2>
-    <textarea
-      name="notes"
-      value={notes || ''}
-      onChange={(e) => { firebase.set('notes', e.target.value); }}
-    />
-  </div>
-);
+  return (
+    <div className="Notes">
+      <h2>Notes</h2>
+      <textarea
+        name="notes"
+        value={notes || ''}
+        onChange={(e) => { firebase.set('notes', e.target.value); }}
+      />
+    </div>
+  );
+};
 
 Notes.propTypes = {
-  notes: PropTypes.string,
+  notes: PropTypes.string.isRequired,
   firebase: PropTypes.shape({
     set: PropTypes.func.isRequired,
   }).isRequired,
-};
-
-Notes.defaultProps = {
-  notes: '',
 };
 
 export default compose(
